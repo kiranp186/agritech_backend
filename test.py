9be2371dece3e28b84f0b4eb914f3d385bd539c8
kiranp186 <kpkiranp186@gmail.com>
2025-05-10 07:58:45 +0530

    test new

--- test.py
+++ test.py
@@ -1,19 +1,31 @@
 import requests
 import json
 
-image_path = "C:/Users/kpkir/Downloads/sick_leaf.jpg"
+# Replace with the local URL where your Flask app is running
+URL = "http://127.0.0.1:5000/predict"
 
+# Image file path (use raw string or forward slashes)
+image_path = "C:\\Users\\kpkir\\Downloads\\potatoblight.jpg"
+#sensor example data
 sensor_data = {
-    "temperature": 32.5,
-    "humidity": 70,
-    "soil_moisture": 300,
-    "light":200
+    "temperature": 28.5,
+    "humidity": 65,
+    "soil_moisture": 450,
+    "light": 300
 }
 
-response = requests.post(
-    "http://localhost:5000/predict",
-    files={"image": open(image_path, "rb")},
-    data={"sensor_data": json.dumps(sensor_data)}
-)
+# Prepare request
+files = {
+    'image': open(image_path, 'rb')
+}
+data = {
+    'sensor_data': json.dumps(sensor_data)
+}
 
-print(response.json())
+# Send POST request
+response = requests.post(URL, files=files, data=data)
+
+# Print response
+print("Status Code:", response.status_code)
+print("Response JSON:")
+print(json.dumps(response.json(), indent=2))
